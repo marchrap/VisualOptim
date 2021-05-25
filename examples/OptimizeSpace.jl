78b50968f36a0d6e38e7655fb8072ce09f0507c9
Marcin Chrapek <mac224@cam.ac.uk>
2021-05-25 20:46:20 +0200

    Document the space optimization

--- examples/OptimizeSpace.jl
+++ examples/OptimizeSpace.jl
@@ -8,11 +8,13 @@ using Images
 using JLD
 using Colors
 
-epsilons = [2.0]
+# Get the array of epsilons
+epsilons = Array(1.0:5.0)
 
+# Optimize each space using the specified model and save the space
 for eps in epsilons
     B = [RGB(i/255, j/255, k/255) for i = 0:255, j = 0:255, k = 0:255]
-    model = Colors.DE_AB()
+    model = CAM16Model()
     optimize_space!(B, eps, model, :yes)
     save("spaces/$eps.jld", "B", B)
 end
